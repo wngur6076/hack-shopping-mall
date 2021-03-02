1189c17c3737498410fcb16671beb66b99f2c7a9
wngur6076 <wngur6076@naver.com>
2021-03-02 19:45:59 +0900

    test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function testBasicTest()
+    {
+
+    }
+}
